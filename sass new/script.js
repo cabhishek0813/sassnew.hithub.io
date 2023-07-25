192ecd80e056f6ecabfab2ce591ac0bc6aa097c3
cabhishek0813 <cabhishek7528@gmail.com>
2023-07-25 13:07:51 +0530

    added

---
+++ sass new/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const btnMenu = document.querySelector(".menu-btn");
+const hamberger = document.querySelector(".menu-btn_burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+
+let showMenu = false;
+
+btnMenu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamberger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+
+    showMenu = true;
+  } else {
+    hamberger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+
+    showMenu = false;
+  }
+}
